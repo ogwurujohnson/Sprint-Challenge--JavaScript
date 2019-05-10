70ff726624d73d454e64f1fc751176b6e27a80af
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-10 09:42:45 +0100

    Feat: Create class CuboidMaker

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,6 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
+class CuboidMaker {
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
