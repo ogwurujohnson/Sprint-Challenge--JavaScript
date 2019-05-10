1d14272f0dd4db3995e31a026fd426a336607ea5
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-10 09:43:47 +0100

    Feat: Create the volume and surface area methods

--- challenges/classes.js
+++ challenges/classes.js
@@ -6,6 +6,25 @@ class CuboidMaker {
     this.width = attributes.width;
     this.height = attributes.height;
   }
+  volume() {
+    return this.length * this.width * this.height;
+  }
+
+  /**
+   * @name surfaceArea()
+   *
+   * @returns
+   * @memberof CubeMaker
+   */
+  surfaceArea() {
+    return (
+      2 *
+      (this.length * this.width +
+        this.length * this.height +
+        this.width * this.height)
+    );
+  }
+}
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
