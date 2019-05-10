750088f1e970e022d1cd9093abad2e5efc60b69e
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-10 09:23:55 +0100

    Feat: Give answer to the question.  - Explain in your own words why `nestedfunction()` can access the variable `internal`

--- challenges/functions.js
+++ challenges/functions.js
@@ -19,7 +19,7 @@ function add(number1, number2) {
   return console.log(number1+number2);
 }
 function multiply(number1, number2) {
-  return console.log(number*number2);
+  return console.log(number1*number2);
 }
 function greeting(firstname, lastname) {
   return console.log(`Hello ${firstname} ${lastname}, nice to meet you!`);
@@ -27,13 +27,19 @@ function greeting(firstname, lastname) {
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: In Javascript, variables defined outside a function are automatically made available 
+// inside the function because Javascript uses something called lexical scoping. When a returned inner funtion isn't self contained
+// but is dependent on the outer function for a variable , then we could refer to that as a closure. Closure is the reason, why the inner function in 
+// our case nestedFunction() is able to access the variable internal.
+
 
 
 const external = "I'm outside the function";
